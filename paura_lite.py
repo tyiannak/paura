fccf884291b62d123fb85d2771e1dfd9d47af6f1
Theodoros Giannakopoulos <tyiannak@gmail.com>
2020-05-31 14:58:18 +0300

    fix number of frequency bins added

--- paura_lite.py
+++ paura_lite.py
@@ -7,9 +7,12 @@ import os
 
 rows, columns = os.popen('stty size', 'r').read().split()
 
+buff_size = 0.2
+wanted_num_of_bins = 40
+
 # initialize soundcard for recording:
 fs = 8000
-mid_buf_size = int(fs * 0.1)
+mid_buf_size = int(fs * buff_size)
 pa = pyaudio.PyAudio()
 stream = pa.open(format=pyaudio.paInt16, channels=1, rate=fs,
                  input=True, frames_per_buffer=mid_buf_size)
@@ -25,8 +28,9 @@ while 1:
     # (and resample to visualize)
     X = np.abs(scp.fft(x))[0:int(seg_len/2)]
     freqs = (np.arange(0, 1 + 1.0/len(X), 1.0 / len(X)) * fs / 2)
-    freqs2 = freqs[0::10].astype('int')
-    X2 = np.mean(X.reshape(-1, 10), axis=1)
+    wanted_step = (int(freqs.shape[0] / wanted_num_of_bins))
+    freqs2 = freqs[0::wanted_step].astype('int')
+    X2 = np.mean(X.reshape(-1, wanted_step), axis=1)
     # plot (freqs, fft) as horizontal histogram
     fig = tpl.figure()
     fig.barh(X2, labels=[str(int(f)) + " Hz" for f in freqs2[0:-1]],
